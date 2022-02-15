007c68bc462946e26c8502238c01594e3a596bc0
John Lumtao(Sea Raider) <arseniolumtao1324@gmail.com>
2022-02-16 07:22:54 +0800

    Add_Student

--- Test.py
+++ Test.py
@@ -1,33 +1,34 @@
+import csv
+namesect = "name,section"
+
 def main():
     # Prints the Main Menu. This is where you input your choice for functions.
     choice = ''
-    while choice.lower() != 'x':
+    while True:
         Print_MainMenu()
         choice = input("Selection: ")
         if choice.lower() == 'a':
             Add_Student()
+        if choice.lower() == 'd':
+            view_students()
+        if choice.lower() == 'x':
+            break
+        
 
 # Function to add student.
-def Add_Student():
-    # Opens the AllStudents text file where all students are located.
-    f = open("AllStudents","at")
+def Print_MainMenu():
+    print("MAIN MENU --")
+    print("(A) Add Student") # Add Student --> Select Section??
+    print("(B) Add Course") # Select Section --> Select Course??
+    print("(C) Add Section") # Add Section
+    print("(D) View Students per Section")
+    print("(X) Exit")
 
-    while(True):
-        student_name_input = input("Enter student name: ")
-
-        # Removes all space to see all characters input are alphanumerical.
-        temp_name_copy = student_name_input
-        temp_name_copy = temp_name_copy.replace(" ","")
-
-        # if isalpha() == true, break loop and write input on AllStudents text file.
-        if temp_name_copy.isalpha():
-            print("written")
-    
-            f.write(student_name_input)
-            f.write("\n")
-            
-            break
-    pass
+def view_students():
+  with open('AllStudents','r') as file:
+    reader = csv.DictReader(file)
+    for i in reader:
+      print(i["name"])
 
 def Add_Course():
     pass
@@ -47,15 +48,77 @@ def View_Student_Section():
 def View_All_Students():
     pass
 
-def Print_MainMenu():
-    print("(A) Add Student") # Add Student --> Select Section??
-    print("(B) Add Course") # Select Section --> Select Course??
-    print("(C) Add Section") # Add Section
-    print("(D) Add Grade Components") # Grade Components
-    print("(E) Grade Computation")
-    print("(F) View Student Grade per Section")
-    print("(G) View All Student")
-    print("(X) Exit")
+def Add_Student():
+    # Opens the AllStudents text file where all students are located.
     
+    choice = ''
+    while True:
+        print("ADD STUDENT --")
+        print("A.a) Add Students")
+        print("B.b) Remove Students")
+        print("B.x) Back")
+        choice = input("Selection: ")
+        if choice.lower() == 'a':
+            A_add_student()
+        elif choice.lower() == 'b':
+            A_remove_student()
+        elif choice.lower() == 'x':
+            print("go here")
+            break
+
+def A_remove_student():
+    studentList = []
+    name = ""
+    with open("AllStudents","r") as f:
+      next(f)
+      print("Name, Section")
+      for i in f:
+        print(i.rstrip())
+        
+      while True:
+        with open("AllStudents","r") as f:
+          reader = csv.DictReader(f)
+          
+          choice = input("Student to Remove: ")
+          
+          for element in reader:
+            name = element["name"]
+            section = element["section"]
+            studentList.append(name)
+
+          for i in studentList:
+            if i == choice:
+              studentList.remove(choice)
+          print(studentList)
+          
+
+          with open("AllStudents","w") as f:
+            
+            writing = csv.writer(f)
+            f.write(namesect)
+            f.write("\n")
+            for i in studentList:
+              if i != choice:
+                writing.writerow([name,section])
+          break
+    
+
+def A_add_student():
+    while True:
+      
+      student_name_input = input("Enter student name: ")
+      student_section_input = input("Enter section: ")
+
+            # Removes all space to see all characters input are alphanumerical.
+      temp_name_copy = student_name_input
+      temp_name_copy = temp_name_copy.replace(" ","")
+
+            # if isalpha() == true, break loop and write input on AllStudents text file.
+      if temp_name_copy.isalpha():
+          print("written")
+      with open("AllStudents","at") as f:
+          writing = csv.writer(f)
+          writing.writerow([student_name_input,student_section_input])
+          break
 
 main()
