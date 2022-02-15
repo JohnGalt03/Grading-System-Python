75d04f37725cff19dae253d6f999623516f4cba9
John Lumtao(Sea Raider) <arseniolumtao1324@gmail.com>
2022-02-15 15:40:17 +0800

    structure

--- Test.py
+++ Test.py
@@ -1,1 +1,33 @@
-print("Changed")
+def main():
+    pass
+
+def Add_Student():
+    pass
+
+def Add_Course():
+    pass
+
+def Add_Section():
+    pass
+
+def Add_Grade_Component():
+    pass
+
+def Compute_Grades():
+    pass
+
+def View_Student_Section():
+    pass
+
+def View_All_Students():
+    pass
+
+def Print_MainMenu():
+    print("(A) Add Student")
+    print("(B) Add Course")
+    print("(C) Add Section")
+    print("(D) Add Grade Components")
+    print("(E) Grade Computation")
+    print("(F) View Student Grade per Section")
+    print("(G) View All Student")
+    print("(X) Add Student")
